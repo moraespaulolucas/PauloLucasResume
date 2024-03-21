4d29abad156979362c29a06aa74aab92890648c0
Paulo Lucas Moraes <moraes.paulolucas@gmail.com>
2024-03-21 10:06:38 -0300

    added tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,12 @@ import 'zone.js';
   selector: 'app-root',
   standalone: true,
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
+    <h1 class="font-bold text-red">Hello world!</h1>
+    <a class="underline hover:text-blue-600" target="_blank" href="https://angular.dev/overview">
       Learn more about Angular
     </a>
   `,
 })
-export class App {
-  name = 'Angular';
-}
+export class App {}
 
 bootstrapApplication(App);
