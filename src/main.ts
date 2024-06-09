0bc4964f939c917b7e50a99fbd932fda022d77c5
Paulo Lucas Moraes <moraes.paulolucas@gmail.com>
2024-06-09 10:03:07 -0300

    remove wheel event directive

--- src/main.ts
+++ src/main.ts
@@ -7,23 +7,11 @@ import { CustomIconComponent } from './components/custom-icon.component';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-@Directive({
-  selector: 'main',
-  standalone: true
-})
-class ChangeMainSection {
-  @HostListener('wheel', ['$event'])
-  handleEvent(e: WheelEvent) {
-      const scrollDirection = e.deltaY < 0 ? 'up' : 'down'
-      console.log('going ' + scrollDirection)
-  }
-}
-
 @Component({
   selector: 'body',
   standalone: true,
   templateUrl: './main.html',
-  imports: [CommonModule, CustomIconComponent, ChangeMainSection],
+  imports: [CommonModule, CustomIconComponent],
 })
 class App {
   constructor(library: FaIconLibrary) {
